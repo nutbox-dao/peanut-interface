ff611e7482ec92fdb98ed2c101b2a3bec6db33be
wangxi <wangxi8206@sina.com>
2020-12-29 14:38:38 +0800

    update contract name

--- src/utils/contractAddress.js
+++ src/utils/contractAddress.js
@@ -41,6 +41,6 @@ export async function tspPoolAddress(){
     return tronWeb2.address.fromHex(res.data.networks['*'].address)
 }
 export async function tspLPPoolAddress(){
-    let res = await axios.get('/TspLPPooling.json')
+    let res = await axios.get('/TspLpPool.json')
     return tronWeb2.address.fromHex(res.data.networks['*'].address)
 }
